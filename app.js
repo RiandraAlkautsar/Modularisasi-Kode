896f31da790107d60d5387170b0e7f8810e4f087
Riandra Alkautsar <63386068+RiandraAlkautsar@users.noreply.github.com>
2020-04-15 22:24:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var aritmatika = require(`./aritmatika.js`)
+
+var a=10, b=3;
+console.log(`${a} + ${b} = ` + aritmatika.tambah(a, b) );
+console.log(` ${a} - ${b} = ` + aritmatika.kurang(a, b) );
+console.log(` ${a} * ${b} = ` + aritmatika.kali(a, b) );
+console.log(` ${a} / ${b} = ` + aritmatika.bagi(a, b) );
+
